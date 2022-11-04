6e8ff6ac4cfb4fdc0ad4e2d94711618419dacd1e
Tom Troyer <tom.troyer@gmail.com>
2022-11-03 23:32:41 -0400

    Initial implementation, incomplete

---
+++ src/main/java/thtroyer/squirrelnoise/SquirrelNoise.java
@@ -0,0 +1,65 @@
+package thtroyer.squirrelnoise;
+
+public class SquirrelNoise {
+    /**
+     * Based on SquirrelNoise5 by Squirrel Eiserloh, implemented in C++.
+     * Original version available here: * http://eiserloh.net/noise/SquirrelNoise5.hpp
+     *
+     * Since Java has no unsigned int used in the original algorithm, a long is being used, which is
+     * regularly masked back to the original 32-bit int size.
+     *
+     * @param positionX unsigned int value between 0 and 4294967295
+     * @param seed unsigned int value between 0 and 4294967295
+     * @return random value between 0 and 4294967295
+     */
+    public static long squirrelNoise5(long positionX, long seed )
+    {
+        if (positionX < 0 || positionX > 4294967295L) {
+            throw new RuntimeException("positionX out of range.");
+        }
+
+        if (seed < 0 || seed > 4294967295L) {
+            throw new RuntimeException("seed out of range.");
+        }
+
+        long bitMask = Long.parseLong("ffffffff", 16);
+
+        long SQ5_BIT_NOISE1 =Long.parseLong("d2a80a3f",16);
+        long SQ5_BIT_NOISE2 =Long.parseLong("a884f197",16);
+        long SQ5_BIT_NOISE3 =Long.parseLong("6C736F4B",16);
+        long SQ5_BIT_NOISE4 =Long.parseLong("B79F3ABB",16);
+        long SQ5_BIT_NOISE5 =Long.parseLong("1b56c4f5",16);
+
+        long mangledBits = positionX;
+        mangledBits *= SQ5_BIT_NOISE1;
+
+        mangledBits += seed;
+
+        mangledBits &= bitMask;
+        mangledBits ^= (mangledBits >> 9);
+
+        mangledBits += SQ5_BIT_NOISE2;
+
+        mangledBits &= bitMask;
+        mangledBits ^= (mangledBits >> 11);
+
+        mangledBits *= SQ5_BIT_NOISE3;
+
+        mangledBits &= bitMask;
+        mangledBits ^= (mangledBits >> 13);
+
+        mangledBits += SQ5_BIT_NOISE4;
+
+        mangledBits &= bitMask;
+        mangledBits ^= (mangledBits >> 15);
+
+        mangledBits *= SQ5_BIT_NOISE5;
+
+        mangledBits &= bitMask;
+        mangledBits ^= (mangledBits >> 17);
+
+        mangledBits &= bitMask;
+
+        return mangledBits;
+    }
+}
---
+++ src/test/java/thtroyer/squirrelnoise/SquirrelNoiseTest.java
@@ -0,0 +1,38 @@
+package thtroyer.squirrelnoise;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static thtroyer.squirrelnoise.SquirrelNoise.squirrelNoise5;
+
+public class SquirrelNoiseTest {
+    /**
+     * These are verified outputs from the original C++ version
+     */
+    @Test
+    public void testSquirrelNoise5() {
+        assertThat(squirrelNoise5(0,0))
+                .isEqualTo(377036288);
+        assertThat(squirrelNoise5(1,0))
+                .isEqualTo(3365260061L );
+        assertThat(squirrelNoise5(2,0))
+                .isEqualTo(3009420505L);
+        assertThat(squirrelNoise5(182382948,0))
+                .isEqualTo(159582615L);
+        assertThat(squirrelNoise5(0,1))
+                .isEqualTo(603375697L);
+        assertThat(squirrelNoise5(1,1))
+                .isEqualTo(2562153792L);
+        assertThat(squirrelNoise5(238198231,129389182))
+                .isEqualTo(2277364147L);
+    }
+
+    @Test
+    public void testSquirrelNoise5Performance() {
+        for (int i = 0; i < 10000000; i++) {
+            squirrelNoise5(i, i+5);
+        }
+    }
+
+}
