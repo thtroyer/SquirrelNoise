91042efd80e43c7d1ca3299f28a2df2c273c2efd
Tom Troyer <tom.troyer@gmail.com>
2022-11-09 23:03:41 -0500

    Random number generator based on SquirrelNoise

--- src/main/java/thtroyer/squirrelnoise/SquirrelRandom.java
+++ src/main/java/thtroyer/squirrelnoise/SquirrelRandom.java
@@ -1,5 +1,44 @@
 package thtroyer.squirrelnoise;
 
 public class SquirrelRandom {
-    //todo
+    private final long seed;
+    private int position;
+
+    public SquirrelRandom(long seed) {
+        position = 0;
+        this.seed = seed;
+    }
+
+    public SquirrelRandom(long seed, int position) {
+        this.position = position;
+        this.seed = seed;
+    }
+
+    /**
+     * Returns next signed int
+     */
+    public int getNextInt() {
+        return (int)SquirrelNoise.squirrelNoise5(position++, seed);
+    }
+
+    /**
+     * Returns next unsigned int (as a long)
+     */
+    public long getNextLong() {
+        return SquirrelNoise.squirrelNoise5(position++, seed);
+    }
+
+    /**
+     * Returns the next 4 bytes
+     * @return byte[4]
+     */
+    public byte[] getNextBytes() {
+        int value = getNextInt();
+        return new byte[] {
+                (byte)(value >>> 24),
+                (byte)(value >>> 16),
+                (byte)(value >>> 8),
+                (byte)value};
+    }
+
 }
